f4bf4edbfc5df681071ea420d407f2106590a2f0
Alex <BadgerAus@proton.me>
2024-01-08 10:49:00 +1100

    added animations to popup-modal, added if toggle for modal and backdrop

--- animation/02-pop-up/script.js
+++ animation/02-pop-up/script.js
@@ -3,9 +3,16 @@ const closeButton = document.getElementById('close-modal');
 
 function toggleModal() {
   const modalDiv = document.querySelector('.popup-modal');
-  const backdrop = document.querySelector('.backdrop');
+  const backdrop = document.querySelector('.backdrop')
+  if (modalDiv.classList.contains('show')) {
+    modalDiv.classList.toggle('hide')
+  }
+  if (backdrop.classList.contains('show')) {
+    backdrop.classList.toggle('hide')
+  }
   modalDiv.classList.toggle('show');
   backdrop.classList.toggle('show');
+
 }
 
 openButton.addEventListener('click', toggleModal);
